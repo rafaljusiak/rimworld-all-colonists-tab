1a028718ce2cd3f835880695f2321648eedfc3be
Rafal Jusiak <jusiak.raf@gmail.com>
2024-08-24 22:06:12 +0200

    Add test label

--- Source/PawnTimeline/Window.cs
+++ Source/PawnTimeline/Window.cs
@@ -7,9 +7,12 @@ namespace PawnTimeline
 
     internal class Window : MainTabWindow
     {
+        private readonly Listing_Standard listing = new Listing_Standard();
+
         public override void DoWindowContents(Rect inRect)
         {
-            throw new System.NotImplementedException();
+            listing.Begin(inRect);
+            listing.Label("Here we go");
         }
     }
 }
