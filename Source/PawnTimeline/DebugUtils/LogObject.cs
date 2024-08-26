b0058e416ad7817d09bde8872ad0314d11f6c33a
Rafal Jusiak <jusiak.raf@gmail.com>
2024-08-26 10:04:00 +0200

    Add DebugUtils

---
+++ Source/PawnTimeline/DebugUtils/LogObject.cs
@@ -0,0 +1,32 @@
+namespace PawnTimeline.DebugUtils;
+
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+public static class LogObject
+{
+    public static string GetLogFor(object target)
+    {
+        var properties =
+            from property in target.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance)
+            select new
+            {
+                Name = property.Name,
+                Value = property.GetValue(target, null)
+            };
+
+        var builder = new StringBuilder();
+
+        foreach (var property in properties)
+        {
+            builder
+                .Append(property.Name)
+                .Append(" = ")
+                .Append(property.Value)
+                .AppendLine();
+        }
+
+        return builder.ToString();
+    }
+}
