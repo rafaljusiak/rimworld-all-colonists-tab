a5916686886b54f4ea7a48607eff8a1c42b88f35
Rafal Jusiak <jusiak.raf@gmail.com>
2024-08-30 23:46:45 +0200

    Nicer window

--- Source/PawnTimeline/Window.cs
+++ Source/PawnTimeline/Window.cs
@@ -3,34 +3,81 @@ using UnityEngine;
 using Verse;
 using System;
 
-using PawnTimeline.DebugUtils;
-
 namespace PawnTimeline
 {
     internal class Window : MainTabWindow
     {
         private readonly Listing_Standard listing = new Listing_Standard();
+        private Vector2 scrollPosition;  // To track the scroll position
+        private Pawn selectedPawn;
 
         public override void DoWindowContents(Rect inRect)
         {
             Text.Font = GameFont.Small;
 
-            listing.Begin(inRect);
+            float leftPanelWidth = inRect.width * 0.33f;
+            float rightPanelWidth = inRect.width * 0.67f;
+
+            Rect leftPanelRect = new Rect(0, 0, leftPanelWidth, inRect.height);
+            Rect rightPanelRect = new Rect(leftPanelWidth, 0, rightPanelWidth, inRect.height);
+
+            Rect scrollRect = new Rect(0, 0, leftPanelWidth, inRect.height - 50);
+            Rect viewRect = new Rect(0, 0, leftPanelWidth - 16, 1000);
+
+            Widgets.BeginScrollView(scrollRect, ref scrollPosition, viewRect);
+
+            listing.Begin(viewRect);
             listing.Label("Alive pawns:");
 
             var pawns = PawnRetriever.GetAlivePlayerPawns();
             foreach (var pawn in pawns)
             {
-                var joinDate = GetJoinDate(pawn);
-                listing.Label(pawn.Name.ToStringFull + " - " + joinDate);
+                if (Widgets.ButtonText(listing.GetRect(Text.LineHeight), pawn.Name.ToStringFull))
+                {
+                    selectedPawn = pawn;
+                }
             }
 
-            var deadPawns = PawnRetriever.GetDeadPlayerPawns();
+            listing.GapLine();
             listing.Label("Dead pawns:");
+
+            var deadPawns = PawnRetriever.GetDeadPlayerPawns();
             foreach (var deadPawn in deadPawns)
             {
-                var joinDate = GetJoinDate(deadPawn);
-                listing.Label(deadPawn.Name.ToStringFull + " - " + joinDate);
+                if (Widgets.ButtonText(listing.GetRect(Text.LineHeight), deadPawn.Name.ToStringFull))
+                {
+                    selectedPawn = deadPawn;
+                }
+            }
+
+            listing.GapLine();
+            listing.Gap();
+
+            listing.End();
+
+            Widgets.EndScrollView();
+
+            DrawPawnDetails(rightPanelRect);
+        }
+
+        private void DrawPawnDetails(Rect rightPanelRect)
+        {
+            listing.Begin(rightPanelRect);
+            if (selectedPawn != null)
+            {
+                listing.Label($"Details for: {selectedPawn.Name.ToStringFull}");
+
+                var joinDate = GetJoinDate(selectedPawn);
+                listing.Label($"Join Date: {joinDate}");
+
+                listing.GapLine();
+                listing.Label($"More details about {selectedPawn.Name.ToStringFull}:");
+
+                listing.Gap();
+            }
+            else
+            {
+                listing.Label("Select a pawn to see details.");
             }
             listing.End();
         }
