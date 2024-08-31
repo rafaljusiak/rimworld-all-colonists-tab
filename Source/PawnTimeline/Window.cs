fa0557d046644dc107a5b977ade157ecdfa4ece2
Rafal Jusiak <jusiak.raf@gmail.com>
2024-08-31 22:31:06 +0200

    Display skills; Add 'Focus' button

--- Source/PawnTimeline/Window.cs
+++ Source/PawnTimeline/Window.cs
@@ -1,7 +1,6 @@
 using RimWorld;
 using UnityEngine;
 using Verse;
-using System;
 using System.Linq;
 using System.Collections.Generic;
 
@@ -24,7 +23,7 @@ namespace PawnTimeline
 
             alivePawns = PawnRetriever.GetAlivePlayerPawns().Select(p => new PawnWithStats(p, tilePosition)).OrderBy(p => p.JoinTick);
             deadPawns = PawnRetriever.GetDeadPlayerPawns().Select(p => new PawnWithStats(p, tilePosition)).OrderBy(
-                p => p.PawnInstance.Corpse == null ? -1 : p.JoinTick  // TODO: don't look at join tick -- look at the now-time_death-time_as_colonist (or smth)
+                p => p.PawnInstance.Corpse == null ? -1 : p.JoinTick
             );
         }
 
@@ -92,7 +91,7 @@ namespace PawnTimeline
                 }
 
                 string pawnName = selectedPawnWithStats.PawnInstance.Name.ToStringFull;
-                listing.Label($"Details for: {pawnName}");
+                listing.Label(pawnName);
 
                 var birthdayDate = selectedPawnWithStats.BirthdayDate;
                 listing.Label($"Born: {birthdayDate}");
@@ -111,10 +110,52 @@ namespace PawnTimeline
 
                 listing.Label($"Time as a colonist: {selectedPawnWithStats.TimeAsColonist}");
 
-                listing.GapLine();
-                listing.Label("More details:");
-
                 listing.Gap();
+                listing.GapLine();
+                listing.Gap();
+
+                listing.Label("Skills:");
+
+                var skills = selectedPawnWithStats.PawnInstance.skills;
+                if (skills != null)
+                {
+                    int skillsPerColumn = 4;
+
+                    for (int i = 0; i < skills.skills.Count; i++)
+                    {
+                        var skill = skills.skills[i];
+                        string skillLabel = skill.def.label.CapitalizeFirst();
+                        int skillLevel = skill.Level;
+
+                        Color skillColor = skillLevel switch
+                        {
+                            >= 12 => Color.green,
+                            >= 6 => Color.yellow,
+                            _ => Color.red
+                        };
+
+                        Rect skillRect = listing.GetRect(Text.LineHeight);
+                        float columnWidth = skillRect.width / 3f;
+                        Rect labelRect = new Rect(skillRect.x, skillRect.y, columnWidth, skillRect.height);
+                        Rect numberRect = new Rect(skillRect.x + columnWidth, skillRect.y, columnWidth, skillRect.height);
+
+                        Widgets.Label(labelRect, $"{skillLabel}: ");
+
+                        GUI.color = skillColor;
+                        Widgets.Label(numberRect, skillLevel.ToString());
+
+                        GUI.color = Color.white;
+
+                        if ((i + 1) % skillsPerColumn == 0 && i != skills.skills.Count - 1)
+                        {
+                            listing.Gap(Text.LineHeight);
+                        }
+                    }
+                }
+                else
+                {
+                    listing.Label("Skills information not available.");
+                }
             }
             else
             {
@@ -140,6 +181,12 @@ namespace PawnTimeline
 
                 var portrait = PortraitsCache.Get(selectedPawnWithStats.PawnInstance, pawnTextureSize, Rot4.South, pawnTextureCameraOffset, 1.28205f);
                 GUI.DrawTexture(pawnRect, portrait);
+
+                Rect buttonRect = new Rect(pawnRect.x, pawnRect.yMax + 5f, pawnRect.width, 24f);
+                if (Widgets.ButtonText(buttonRect, "Focus"))
+                {
+                    CameraJumper.TryJumpAndSelect(selectedPawnWithStats.PawnInstance);
+                }
             }
         }
     }
